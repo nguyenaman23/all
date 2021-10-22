aad3ffc27ed1c59252d92779b0fb1e8d41e7fe45
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-22 15:54:24 +0700

    Update cfg.php

--- ltcminer/cfg.php
+++ ltcminer/cfg.php
@@ -1,4 +1,4 @@
 <?php
 
-$cookie = "cf_clearance=kIVLjSX.NQlsGCUrGbKAql_5Ecj3jWG7uioEtdhzuf0-1633253097-0-150; PHPSESSID=4e65ef53071a64adc39fcc9e7bb33656; referral=6721";
+$cookie = "PHPSESSID=b9f352f4a9c4c421f281f4178ef31c81; referral=6839";
 $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36";

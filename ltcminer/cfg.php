c3ff050b656d12b886f759fe95131cbc930ff02d
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-22 14:24:47 +0700

    Add files via upload

---
+++ ltcminer/cfg.php
@@ -0,0 +1,4 @@
+<?php
+
+$cookie = "cf_clearance=kIVLjSX.NQlsGCUrGbKAql_5Ecj3jWG7uioEtdhzuf0-1633253097-0-150; PHPSESSID=4e65ef53071a64adc39fcc9e7bb33656; referral=6721";
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36";
